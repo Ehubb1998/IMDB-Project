3d881ff9f8ddcb60658d795b81c7645ca85822f7
Elijah <ehubb15@gmail.com>
2021-03-09 09:43:43 -0500

    added code for heroku deployment

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,38 @@ app.use("/movies", movieRouter);
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "pug");
 
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static('../../frontend/build'));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.status(201).json({});
+  });
+}
+
 // app.get("/sign-up", (req, res) => {
 //     res.render("sign-up")
 // });
@@ -37,3 +69,7 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
+
+
+// package.json start script that was removed
+// "start": "nodemon -r dotenv/config ./bin/www",
